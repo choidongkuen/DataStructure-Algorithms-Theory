4b4abc441e06d1df8623d3a39d080c3411a8191e
choidongkuen <danaver12@daum.net>
2022-09-16 16:16:59 +0900

    9/16 commit

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,31 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) throws Exception {
+    public static void func(String s, String t){
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        boolean is = true;
+//        System.out.println(Arrays.toString(sArray));
+//        System.out.println(Arrays.toString(tArray));
 
+        for(int i = 0; i < sArray.length; i++){
+            if (sArray[i] != tArray[i]){
+                is = false;
+                break;
+            }
+        }
+        System.out.println(is);
+    }
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int s = Integer.parseInt(br.readLine());
-        String s2 = br.readLine();
-        int N = Integer.parseInt(br.readLine());
-
-        System.out.println(s);
-        System.out.println(s2);
-
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        func(s,t);
     }
 }
